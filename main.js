bb4f22d62f58e0d2ba40167a3f171624ea4bd788
brittwoodard <96916819+brittwoodard@users.noreply.github.com>
2023-06-20 09:17:29 +0800

    finish

--- main.js
+++ main.js
@@ -52,7 +52,6 @@ const headings = document.querySelectorAll(".scroll-in-from-bottom")
 
 // for each of them we are going to attach a gsap animation ( like a jekyll for loop just in js :D )
 headings.forEach(object => {
-
 // we start with a gsap timeline
     let tl = gsap.timeline({
     // here's where we start attching the animation to the scroll
@@ -62,13 +61,16 @@ headings.forEach(object => {
         	// this one takes in to bits of info. the first is the part of the trigger element thats causing the trigger to start and the second one is where on the screen. both of them can be either top, center, bottom or a percentage (% are from the top of the container( as in screen or trigger element) )   
           start: "top bottom",
 			// same as start but the end point of the animation          
-          end: "top center",
+          end: "top 85%",
           // now this is the most important line of code its what turns the triggers from a start point in to locking it to the scroll
           scrub: true,
           // last but not least this helps us to visualize the script
           markers: true,
       }
     }).from(object,{
-        bottom:-300,
+        y: 300,
         opacity:0
     })
+  })
+
+  console.log('dan was here')
